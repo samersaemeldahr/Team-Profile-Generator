c95854d0f90a910718d3afef3ff77254c5a72614
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-04-18 00:57:37 +0300

    pass tests for manager class

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,1 +1,18 @@
-const Manager = require("../lib/manager");
+const Manager = require('../lib/manager')
+
+describe("officeNumber", () => {
+    it("Should return the manager's office number", () => {
+        let call = new Manager();
+        call.officeNumber = 1231231234;
+
+        expect(call.getOfficeNumber()).toEqual(1231231234);
+    })
+})
+
+describe("getRole", () => {
+    it("Should return 'Manager'", () => {
+        let position = new Manager("Manager");
+
+        expect(position.getRole()).toEqual("Manager");
+    })
+})
