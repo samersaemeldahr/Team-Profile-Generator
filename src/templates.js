6f6005581f5334f60b566f21cb4db441c9ca6591
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-04-22 05:40:46 +0300

    combine all template functions into one

--- src/templates.js
+++ src/templates.js
@@ -1,6 +1,188 @@
-function mainHTML(answers) {
-    `
-<!DOCTYPE html>
+// const Intern = require('../lib/intern');
+// const manager = require('../lib/manager');
+// const Engineer = require('../lib/engineer');
+// const Employee = require('../lib/employee');
+
+// function mainHTML(employee) {
+//     if (employee) {
+//         return `
+// <!DOCTYPE html>
+// <html>
+
+// <head>
+//     <meta charset="utf-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <title></title>
+//     <meta name="description" content="">
+//     <meta name="viewport" content="width=device-width, initial-scale=1">
+//     <link rel="stylesheet" href="style.css">
+//     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+//         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+//         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+// </head>
+
+// <body>
+
+//     <div class="jumbotron text-center bg-danger">
+//         <h1>My Team</h1>
+//     </div>
+
+//     <div class="container d-flex flex-wrap justify-content-center">
+//     <div class="card border-dark m-3">
+//             <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
+//                     class="fas fa-user-tie"> Manager</span></div>
+//             <div class="card-body text-dark">
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">ID: ${employee.id}</li>
+//                     <li class="list-group-item">Email: ${employee.email}</li>
+//                     <li class="list-group-item">Office number: ${employee.officeNumber}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//     </div>
+
+
+// `} else if (employee.getRole() === "Engineer") {
+//         return `
+//     <div class="card border-dark m-3">
+//             <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
+//                     class="fas fa-user-cog"> Engineer</span></div>
+//             <div class="card-body text-dark">
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">ID: ${employee.id}</li>
+//                     <li class="list-group-item">Email: ${employee.email}</li>
+//                     <li class="list-group-item">GitHub: ${employee.github}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//         `
+//     } else if (employee.getRole() === "Intern") {
+//         return `<div class="card border-dark m-3">
+//     <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
+//             class="fas fa-user-graduate"> Intern</span></div>
+//     <div class="card-body text-dark">
+//         <ul class="list-group list-group-flush">
+//             <li class="list-group-item">ID: ${employee.id}</li>
+//             <li class="list-group-item">Email: ${employee.email}</li>
+//             <li class="list-group-item">School: ${employee.school}</li>
+//         </ul>
+//     </div>
+// </div>
+// `
+//     } else {
+//         return `
+//         </body>
+
+// </html>`
+//     }
+// }
+
+
+// function engineerTemplate(employee) {
+//     return `
+//     <div class="card border-dark m-3">
+//             <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
+//                     class="fas fa-user-cog"> Engineer</span></div>
+//             <div class="card-body text-dark">
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">ID: ${employee.id}</li>
+//                     <li class="list-group-item">Email: ${employee.email}</li>
+//                     <li class="list-group-item">GitHub: ${employee.github}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//         `
+// }
+
+// function internTemplate(employee) {
+//     return `<div class="card border-dark m-3">
+//     <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
+//             class="fas fa-user-graduate"> Intern</span></div>
+//     <div class="card-body text-dark">
+//         <ul class="list-group list-group-flush">
+//             <li class="list-group-item">ID: ${employee.id}</li>
+//             <li class="list-group-item">Email: ${employee.email}</li>
+//             <li class="list-group-item">School: ${employee.school}</li>
+//         </ul>
+//     </div>
+// </div>
+// `
+// }
+
+// // function createHTML(employee) {
+// //     mainHTML()
+// //     cssTemplate();
+
+// //     if (employee.getRole() === "Engineer") {
+// //         return engineerTemplate(employee);
+// //     } else if (employee.getRole() === "Intern") {
+// //         return internTemplate(employee)
+// //     }
+// // }
+
+
+
+// // createHTML = (data) => {
+
+// //     // array for cards 
+// //     pageArray = []; 
+
+// //     for (let i = 0; i < data.length; i++) {
+// //         const employee = data[i];
+// //         const role = employee.getRole(); 
+
+
+// //         // call manager function
+// //         if (role === 'Manager') {
+// //             const managerCard = generateManager(employee);
+
+// //             pageArray.push(managerCard);
+// //         }
+
+// //         // call engineer function
+// //         if (role === 'Engineer') {
+// //             const engineerCard = generateEngineer(employee);
+
+// //             pageArray.push(engineerCard);
+// //         }
+
+// //         // call intern function 
+// //         if (role === 'Intern') {
+// //             const internCard = generateIntern(employee);
+
+// //             pageArray.push(internCard);
+// //         }
+
+// //     }
+
+// //     // joining strings 
+// //     const employeeCards = pageArray.join('')
+
+// //     // return to generated page
+// //     const generateTeam = generateTeamPage(employeeCards); 
+// //     return generateTeam;
+
+// // }
+
+// module.exports = {
+//     mainHTML,
+//     //    mainHTML,
+//     //  managerTemplate,
+//     //internTemplate,
+//     //  engineerTemplate
+// }
+
+function createHtml(employee) {
+    // finalize HTML
+    if (!employee) {
+        return `
+        </div>
+</body>
+</html>`
+        //manager
+    } else if (employee.getRole() === "Manager") {
+        return `<!DOCTYPE html>
 <html>
 
 <head>
@@ -23,102 +205,47 @@ function mainHTML(answers) {
     </div>
 
     <div class="container d-flex flex-wrap justify-content-center">
-        
-    </div>
-
-</body>
-
-</html>
-`
-}
-
-function managerTemplate() {
-    `
-    <div class="card border-dark m-3">
-            <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">Header<br><span
+        <div class="card border-dark m-3">
+            <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
                     class="fas fa-user-tie"> Manager</span></div>
             <div class="card-body text-dark">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: </li>
-                    <li class="list-group-item">Email: </li>
-                    <li class="list-group-item">Office number: </li>
+                    <li class="list-group-item">ID: ${employee.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                    <li class="list-group-item">Office number: ${employee.officeNumber}</li>
                 </ul>
             </div>
         </div>
-        `
-}
-
-function engineerTemplate() {
-    `
+`
+        //engineer
+    } else if (employee.getRole() === "Engineer") {
+        return `
     <div class="card border-dark m-3">
-            <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">Header<br><span
+            <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
                     class="fas fa-user-cog"> Engineer</span></div>
             <div class="card-body text-dark">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: </li>
-                    <li class="list-group-item">Email: </li>
-                    <li class="list-group-item">GitHub: </li>
+                    <li class="list-group-item">ID: ${employee.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                    <li class="list-group-item">GitHub: <a href="${employee.github}">${employee.github}</a></li>
                 </ul>
             </div>
-        </div>
-        `
-}
-
-function internTemplate() {
-    `<div class="card border-dark m-3">
-    <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">Header<br><span
+        </div>`
+        //intern
+    } else if (employee.getRole() === "Intern") {
+        return `
+        <div class="card border-dark m-3">
+    <div class="card-header bg-primary header-color" style="font-size: 20px; font-weight: bold;">${employee.name}<br><span
             class="fas fa-user-graduate"> Intern</span></div>
     <div class="card-body text-dark">
         <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: </li>
-            <li class="list-group-item">Email: </li>
-            <li class="list-group-item">School: </li>
+            <li class="list-group-item">ID: ${employee.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+            <li class="list-group-item">School: ${employee.school}</li>
         </ul>
     </div>
-</div>
-`
+</div>`
+    }
 }
 
-function cssTemplate() {
-    `* {
-        box-sizing: border-box;
-    }
-    
-    .jumbotron {
-        color: mintcream;
-        padding: 4% 0 4% 0;
-    };
-    
-    .header-color {
-        color: mintcream;
-    }
-    
-    .card {
-        color: mintcream;
-        width: 14rem;
-        max-width: 28rem;
-    }
-    
-    .container {
-        display: flexbox;
-    }
-    
-    @media screen and (max-width: 768px) {
-        .card {
-            width: unset;
-            max-width: unset;
-        }
-    }
-    
-    @media screen and (max-width: 768px) {
-        .container {
-          display: block;
-          flex-direction: column;
-        }
-    }
-    `
-
-}
-
-
-
+module.exports = createHtml
