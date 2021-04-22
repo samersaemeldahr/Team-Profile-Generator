bf515135c17a3237a387ae55d8b13368b12d5cd8
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-04-22 05:02:34 +0300

    convert questions functions to variables

--- src/questions.js
+++ src/questions.js
@@ -1,203 +1,190 @@
-const inquirer = require("inquirer");
-const init = require("../index");
-
-function askQuestions() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "managerName",
-            message: "What's the team manager's name?",
-            validate: managerName => {
-                if (managerName) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid name <<");
-                    return false;
-                }
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What's the team manager's name?",
+        validate: name => {
+            if (name) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid name <<");
+                return false;
             }
-        },
-        {
-            type: "input",
-            name: "managerId",
-            message: "What's the team manager's ID?",
-            validate: managerId => {
-                if (managerId) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid number <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "managerEmail",
-            message: "What's the team manager's email?",
-            validate: managerEmail => {
-                if (managerEmail) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid email address <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "managerNumber",
-            message: "What's the team manager's office number?",
-            validate: managerNumber => {
-                if (managerNumber !== isNaN) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid phone number <<");
-                    return false;
-                }
-            }
-        },
-    ]).then(function () {
-        teamMember()
-    })
-}
-
-
-function teamMember() {
-    inquirer.prompt(
-        {
-            type: "list",
-            name: "addMember",
-            message: "Which type of team member would you like to add?",
-            choices: ["Engineer", "Intern", "I don't want add any more team members"],
         }
-    ).then(data => {
-        if (data.addMember === "Engineer") {
-            engineerQuestions()
-        } else if (data.addMember === "Intern") {
-            internQuestions()
-        } else {
-            init();
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's the team manager's ID?",
+        validate: id => {
+            if (id) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid number <<");
+                return false;
+            }
         }
-    })
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's the team manager's email?",
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid email address <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What's the team manager's office number?",
+        validate: officeNumber => {
+            if (officeNumber !== isNaN) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid phone number <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "teamMember",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want add any more team members"],
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What's your engineer's name?",
+        validate: name => {
+            if (name) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid name <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's your engineer's ID?",
+        validate: id => {
+            if (id) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid number <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your engineer's email?",
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid email address <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What's your engineer's GitHub username?",
+        validate: github => {
+            if (github) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid username <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "teamMember",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want add any more team members"],
+    }
+]
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What's your intern's name?",
+        validate: name => {
+            if (name) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid name <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What's your intern's ID?",
+        validate: id => {
+            if (id) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid number <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your intern's email?",
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid email address <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What's your intern's school?",
+        validate: school => {
+            if (school) {
+                return true;
+            } else {
+                console.log(">> Please enter a valid username <<");
+                return false;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "teamMember",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want add any more team members"],
+    }
+]
+
+
+
+module.exports = {
+    managerQuestions,
+    engineerQuestions,
+    internQuestions
 }
-
-
-function engineerQuestions() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "engineerName",
-            message: "What's your engineer's name?",
-            validate: engineerName => {
-                if (engineerName) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid name <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "engineerId",
-            message: "What's your engineer's ID?",
-            validate: engineerId => {
-                if (engineerId) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid number <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "engineerEmail",
-            message: "What's your engineer's email?",
-            validate: engineerEmail => {
-                if (engineerEmail) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid email address <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "engineerGithub",
-            message: "What's your engineer's GitHub username?",
-            validate: engineerGithub => {
-                if (engineerGithub) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid username <<");
-                    return false;
-                }
-            }
-        },
-    ]).then(function () {
-        teamMember()
-    })
-}
-
-
-function internQuestions() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "internName",
-            message: "What's your intern's name?",
-            validate: internName => {
-                if (internName) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid name <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "internId",
-            message: "What's your intern's ID?",
-            validate: internId => {
-                if (internId) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid number <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "internEmail",
-            message: "What's your intern's email?",
-            validate: internEmail => {
-                if (internEmail) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid email address <<");
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input",
-            name: "internSchool",
-            message: "What's your intern's school?",
-            validate: internSchool => {
-                if (internSchool) {
-                    return true;
-                } else {
-                    console.log(">> Please enter a valid username <<");
-                    return false;
-                }
-            }
-        },
-    ]).then(function () {
-        teamMember()
-    })
-}
-
-module.exports = askQuestions
